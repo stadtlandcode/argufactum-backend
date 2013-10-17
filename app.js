0ee2ea108835cefa1f0120692764feecc853749e
Felix Ebert <felix.ebert@if-core.de>
2013-10-17 17:37:44 +0200

    get, post, cors headers, errorHandler

--- app.js
+++ app.js
@@ -1,9 +1,46 @@
-var express = require('express');
+var express = require('express'), JsonStorage = require('./lib/JsonStorage.js');
 var app = express();
+var storage = new JsonStorage();
 
-app.get('/', function(req, res) {
-	res.send('Hello World');
+// Routing
+app.all('*', function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'Content-Type');
+	next();
 });
 
-app.listen(8444);
-console.log('Listening on port 8444');
+app.get('/topic/:id', function(req, res, next) {
+	storage.get(req.params.id, function(err, doc) {
+		if (err)
+			throw err;
+		res.send(doc ? doc : 404);
+	});
+});
+
+app.post('/topic', function(req, res, next) {
+	storage.add(req.body, function(err, doc) {
+		if (err)
+			throw err;
+		res.send(doc._id);
+	});
+});
+
+// Middleware
+var errorHandler = function(err, req, res, next) {
+	if (!err)
+		return next();
+	console.error(err.stack);
+	res.send(500, {
+		error: '500'
+	});
+};
+
+app.configure(function() {
+	app.use(app.router);
+	app.use(errorHandler);
+});
+
+// Start
+app.listen(8444, 'localhost');
+console.log('Listening on localhost:8444');
